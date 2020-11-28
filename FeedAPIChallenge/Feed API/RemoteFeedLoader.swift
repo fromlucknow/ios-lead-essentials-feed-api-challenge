876d77c2acb208d9c20320f5f14c19a553a2b486
Saifur <saifurrahman3126@gmail.com>
2020-11-28 17:50:51 +0530

    Deliver invalid data for non 200 http responses

--- FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
+++ FeedAPIChallenge/Feed API/RemoteFeedLoader.swift
@@ -20,7 +20,15 @@ public final class RemoteFeedLoader: FeedLoader {
 	
 	public func load(completion: @escaping (FeedLoader.Result) -> Void) {
 		client.get(from: url) { (result) in
-			completion(.failure(RemoteFeedLoader.Error.connectivity))
+			switch result{
+			case let .success(data,response):
+				if response.statusCode != 200{
+					completion(.failure(RemoteFeedLoader.Error.invalidData))
+				}
+			case .failure(_):
+				completion(.failure(RemoteFeedLoader.Error.connectivity))
+
+			}
 		}
 	}
 }
